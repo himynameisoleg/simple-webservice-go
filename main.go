6cd217ba7cffc443ac3566abd88a04d18c2fe23d
oleg <perchykoleg@gmail.com>
2020-03-21 22:09:20 -0500

    sec 6, topic interfaces

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/himynameisoleg/go-getting-started/models"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
-	fmt.Println(u)
+	port := 3000
+	_, err := startWebServer(port, 2)
+	fmt.Println(err)
+}
+
+func startWebServer(port int, numberOfRetries int) (int, error) {
+	fmt.Println("Starting server ...")
+	fmt.Println("Server started on port", port)
+	fmt.Println("Number of retries", numberOfRetries)
+
+	return port, nil
 }
